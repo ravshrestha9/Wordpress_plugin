22700ace34cebc3532f97853095b52a2a2d87424
Shrestha <ravi@truman.edu>
2018-12-21 11:46:08 -0600

    Rearrange file structures

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package   Plugin
+ * @package Plugin
  */
 /*
 Plugin Name: Plugin
@@ -37,69 +37,9 @@ if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
-use Inc\Activate;
-use Inc\Deactivate;
-use Inc\Admin\AdminPages;
+define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-if ( !class_exists( 'Plugin' ) ) {
-
-	class Plugin
-	{
-
-		public $plugin;
-
-		function __construct() {
-			$this->plugin = plugin_basename( __FILE__ );
-		}
-
-		function register() {
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
-
-			add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-
-			add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
-		}
-
-		public function settings_link( $links ) {
-			$settings_link = '<a href="admin.php?page= _plugin">Settings</a>';
-			array_push( $links, $settings_link );
-			return $links;
-		}
-
-		public function add_admin_pages() {
-			add_menu_page( 'Plugin', ' ', 'manage_options', ' _plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-		}
-
-		public function admin_index() {
-			require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
-		}
-
-		protected function create_post_type() {
-			add_action( 'init', array( $this, 'custom_post_type' ) );
-		}
-
-		function custom_post_type() {
-			register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
-		}
-
-		function enqueue() {
-			// enqueue all our scripts
-			wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__ ) );
-			wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__ ) );
-		}
-
-		function activate() {
-			Activate::activate();
-		}
-	}
-
-	$Plugin = new Plugin();
-	$Plugin->register();
-
-	// activation
-	register_activation_hook( __FILE__, array( $Plugin, 'activate' ) );
-
-	// deactivation
-	register_deactivation_hook( __FILE__, array( 'Deactivate', 'deactivate' ) );
-
+if ( class_exists( 'Inc\\Init' ) ) {
+	Inc\Init::register_services();
 }
