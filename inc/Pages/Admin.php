3e6d8c374f6d6816761aa48c04b6601109fc9ffe
Shrestha <ravi@truman.edu>
2019-01-02 15:41:56 -0600

    Work on the post types and front-end

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -1,24 +1,146 @@
 <?php 
 /**
- * @package  Plugin
+ * @package Plugin
  */
-namespace Inc\Admin;
+namespace Inc\Pages;
+
+use Inc\Api\SettingsApi;
+use Inc\Base\BaseController;
+use Inc\Api\Callbacks\AdminCallbacks;
 
 /**
 * 
 */
-class AdminPages
+class Admin extends BaseController
 {
-	
-	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+	public $settings;
+
+	public $callbacks;
+
+	public $pages = array();
+
+	public $subpages = array();
+
+	public function register() 
+	{
+		$this->settings = new SettingsApi();
+
+		$this->callbacks = new AdminCallbacks();
+
+		$this->setPages();
+
+		$this->setSubpages();
+
+		$this->setSettings();
+		$this->setSections();
+		$this->setFields();
+
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 
-	public function add_admin_pages() {
-		add_menu_page( 'Plugin', 'Plugin', 'manage_options', 'plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+	public function setPages() 
+	{
+		$this->pages = array(
+			array(
+				'page_title' => 'Plugin', 
+				'menu_title' => '', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'plugin', 
+				'callback' => array( $this->callbacks, 'adminDashboard' ), 
+				'icon_url' => 'dashicons-store', 
+				'position' => 110
+			)
+		);
 	}
 
-	public function admin_index() {
-		require_once PLUGIN_PATH . 'templates/admin.php';
+	public function setSubpages()
+	{
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'cpt', 
+				'callback' => array( $this->callbacks, 'adminCpt' )
+			),
+			array(
+				'parent_slug' => 'plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'taxonomies', 
+				'callback' => array( $this->callbacks, 'adminTaxonomy' )
+			),
+			array(
+				'parent_slug' => 'plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'widgets', 
+				'callback' => array( $this->callbacks, 'adminWidget' )
+			)
+		);
+	}
+
+	public function setSettings()
+	{
+		$args = array(
+			array(
+				'option_group' => 'options_group',
+				'option_name' => 'text_example',
+				'callback' => array( $this->callbacks, 'OptionsGroup' )
+			),
+			array(
+				'option_group' => 'options_group',
+				'option_name' => 'first_name'
+			)
+		);
+
+		$this->settings->setSettings( $args );
+	}
+
+	public function setSections()
+	{
+		$args = array(
+			array(
+				'id' => 'admin_index',
+				'title' => 'Settings',
+				'callback' => array( $this->callbacks, 'AdminSection' ),
+				'page' => 'plugin'
+			)
+		);
+
+		$this->settings->setSections( $args );
+	}
+
+	public function setFields()
+	{
+		$args = array(
+			array(
+				'id' => 'text_example',
+				'title' => 'Text Example',
+				'callback' => array( $this->callbacks, 'TextExample' ),
+				'page' => 'plugin',
+				'section' => 'admin_index',
+				'args' => array(
+					'label_for' => 'text_example',
+					'class' => 'example-class'
+				)
+			),
+			array(
+				'id' => 'first_name',
+				'title' => 'First Name',
+				'callback' => array( $this->callbacks, 'FirstName' ),
+				'page' => 'plugin',
+				'section' => 'admin_index',
+				'args' => array(
+					'label_for' => 'first_name',
+					'class' => 'example-class'
+				)
+			)
+		);
+
+		$this->settings->setFields( $args );
 	}
 }
--- plugin.php
+++ plugin.php
@@ -37,8 +37,21 @@ if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
-define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+/**
+ * The code that runs during plugin activation
+ */
+function activate_plugin() {
+	Inc\Base\Activate::activate();
+}
+register_activation_hook( __FILE__, 'activate_plugin' );
+
+/**
+ * The code that runs during plugin deactivation
+ */
+function deactivate_plugin() {
+	Inc\Base\Deactivate::deactivate();
+}
+register_deactivation_hook( __FILE__, 'deactivate_plugin' );
 
 if ( class_exists( 'Inc\\Init' ) ) {
 	Inc\Init::register_services();
